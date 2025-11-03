d8cdac89079110e2d3f6ab2ba7f05eef493edbe8
anointing1234 <yakubudestiny9@gmail.com>
2025-11-03 21:50:00 +0100

    commted

--- teaspace_p/settings.py
+++ teaspace_p/settings.py
@@ -183,12 +183,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
 DEFAULT_FROM_EMAIL = "info@taesaerospace.com" 
 
-RESEND_API_KEY = os.getenv("RESEND_API_KEY")
+# RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 
-if not RESEND_API_KEY:
-    print("❌ RESEND_API_KEY not found in environment!")
-else:
-    print("✅ RESEND_API_KEY loaded successfully")
+# if not RESEND_API_KEY:
+#     print("❌ RESEND_API_KEY not found in environment!")
+# else:
+#     print("✅ RESEND_API_KEY loaded successfully")
 
 
 
